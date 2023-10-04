d1173d4c00ca727a6131aae07a4cc9250e95a0b8
entbappy <entbappy73@gmail.com>
2023-10-04 12:17:34 +0600

    updated

--- app.py
+++ app.py
@@ -44,6 +44,6 @@ def predictRoute():
 if __name__ == "__main__":
     clApp = ClientApp()
     # app.run(host='0.0.0.0', port=8080) #local host
-    # app.run(host='0.0.0.0', port=8080) #for AWS
-    app.run(host='0.0.0.0', port=80) #for AZURE
+    app.run(host='0.0.0.0', port=8080) #for AWS
+    # app.run(host='0.0.0.0', port=80) #for AZURE
 
